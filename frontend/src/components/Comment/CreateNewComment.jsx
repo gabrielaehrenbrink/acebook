adc95b69234b6222ae7a74103639a1d13c12fd3d
Demetrius Vissarion <demetrius.vissarion@gmail.com>
2024-02-08 11:57:41 +0000

    merge from main

--- frontend/src/components/Comment/CreateNewComment.jsx
+++ frontend/src/components/Comment/CreateNewComment.jsx
@@ -62,7 +62,9 @@ const CreateNewComment = (props) => {
                 onKeyPress={(e) => {
                     if (e.key === 'Enter' && !e.shiftKey) {
                         e.preventDefault();
-                        handleSubmit(e);
+                        if (text.trim() !== '') {
+                            handleSubmit(e);
+                        }
                     }
                 }}
                 placeholder="Write a comment..."
